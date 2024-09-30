8996a6682b6d44b885cd118bd66b391400155807
carol <00001095845123sp@aluno.educacao.sp.gov.br>
2024-09-30 11:47:06 -0300

    na pasta tem um arquivo projeto para um codigo de mensagem de amor

---
+++ mensagem de amor.py
@@ -0,0 +1,8 @@
+
+
+from colorama import Fore, Style
+
+def mensagem_de_amor(Carol):
+    return f"Querida OLivia, você é a razão do meu sorriso. {Style.RESET_ALL}"
+
+print(mensagem_de_amor("Amor voce escolheu Opção A"))
